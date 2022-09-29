1437181c5b228e994cb24d8ad6bd92f1c8a06072
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-09-29 03:02:04 -0400

    Revert "feature/TA-6: Delete User"
    This reverts commit 44b74c2fcb4430b0179f45fde841beb40ac8b456.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,21 +6,6 @@ const jsonwebtoken = require('jsonwebtoken');
 const constants = require('../config/constants.json');
 const bcrypt = require('bcryptjs');
 
-const auth = (request, response, next) => {
-    const token = request.header('x-auth-token');
-
-    if(!token){
-        return response.status(401).json({message: 'No token, authorization denied'});
-    }
-    try {
-        const decoded = jsonwebtoken.verify(token, constants.jsonwebtokenSecret);
-        request.userID = decoded.userID;
-        next();
-    } catch(error) {
-        response.status(401).json({message: 'Token is not valid'});
-    }
-}
-
 //http://localhost:8000/api/auth
 
 /*
