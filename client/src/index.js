4c3e8f3048bec871d1129e41f3cb0ff0b5216999
Shun Murai <94808195+Shunm634-source@users.noreply.github.com>
2022-10-01 18:26:44 -0400

    feature: Created the signup react page under the client folder. (#24)
    TA-7
    * added signup page under client with error checking
    
    Co-authored-by: Nandhakishore K.S <n.krishnamurthy@mail.utoronto.ca>
    Co-authored-by: Andrew Qian <andrewqian2001@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Signup from './Signup';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Signup />
 );
 
 // If you want to start measuring performance in your app, pass a function
