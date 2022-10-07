6519e421528381e8ee990266b9d681b216a4fbe4
Henry-Zhuo <henryzhuo@live.com>
2022-10-07 19:16:56 -0400

    TA-15: Clean up code styling in profileInfo.jsx

--- client/src/Components/profileInfo.jsx
+++ client/src/Components/profileInfo.jsx
@@ -6,50 +6,32 @@ import { getEmail, getUser } from '../Utils/Common'
 
 const ProfileInfo = () => {
     
-  const user = getUser();
+    const user = getUser();
+    const email = getEmail();
 
-  const email = getEmail();
+    return (
+        <>
+            <div>
+                welcome back {user}<pre></pre> your email: {email}
+            </div>
 
-  return (
-    <>
+            <div>
+                <img src={coverIMG} width={927} height={90} alt="cover" />
+            </div>
 
-         <div>
-             welcome back {user}<pre></pre> your email: {email}
-        </div>
-    
-  
-        
-        <div>
-        {<img src={coverIMG} width={927} height={90} alt="cover"   />}
-        </div>
-
-           
-        <div style={{
-            backgroundColor: "#D3D3D3",
-        
-            height:"134px"
-        }}>
+            <div 
+                style={{
+                    backgroundColor: "#D3D3D3",
+                    height:"134px",
+                }}
+            >
           
-
-            {<img src={profilepic} height={120} alt="profile-pic"   />}
-
+            <img src={profilepic} height={120} alt="profile-pic" />
                 <Button variant = "outlined" size="medium" color = "primary" >edit cover</Button>
-        
-                 <Button variant = "outlined" size="medium" color = "primary">edit profile  </Button>
-           
-        </div>
-
-      
-        
-        
-
-
-
-        
-
-    </>
-   
-  )
+                <Button variant = "outlined" size="medium" color = "primary">edit profile</Button>
+            </div>
+        </>
+    )
 }
 
 export default ProfileInfo
